f7085006b706f3d650cb7c3a9e12a1fa4338db18
Jared Lewis <jared@jared.kiwi.nz>
2025-07-02 15:38:24 +1000

    chore: Fix formatting

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -11,33 +11,32 @@ client.setConfig({ baseUrl: import.meta.env.VITE_BASE_URL });
 const queryClient = new QueryClient();
 
 const router = createRouter({
-	routeTree,
-	context: {
-		queryClient,
-	},
-	defaultPreload: "intent",
-	// Since we're using React Query, we don't want loader calls to ever be stale
-	// This will ensure that the loader is always called when the route is preloaded or visited
-	defaultPreloadStaleTime: 0,
-	scrollRestoration: true,
+  routeTree,
+  context: {
+    queryClient,
+  },
+  defaultPreload: "intent",
+  // Since we're using React Query, we don't want loader calls to ever be stale
+  // This will ensure that the loader is always called when the route is preloaded or visited
+  defaultPreloadStaleTime: 0,
+  scrollRestoration: true,
 });
 
 declare module "@tanstack/react-router" {
-	interface Register {
-		router: typeof router;
-	}
+  interface Register {
+    router: typeof router;
+  }
 
-	interface StaticDataRouteOption {
-		title?: string;
-	}
+  interface StaticDataRouteOption {
+    title?: string;
+  }
 }
 
-// biome-ignore lint/style/noNonNullAssertion: <explanation>
+// biome-ignore lint/style/noNonNullAssertion: Root element is guaranteed to exist
 createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<RouterProvider router={router} />
-		</QueryClientProvider>
-	</StrictMode>,
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </StrictMode>,
 );
-
