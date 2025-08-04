df6a750c286bf7730601de1e1328b0f81345e305
Jared Lewis <jared@jared.kiwi.nz>
2025-08-04 23:56:07 +1000

    Revert "chore: More speedups"
    This reverts commit 23fce10987c42d67a52a555e1a71b979c8167add.

--- backend/src/ref_backend/core/ref.py
+++ backend/src/ref_backend/core/ref.py
@@ -13,39 +13,23 @@ def get_ref_config(settings: Settings) -> Config:
     return Config.load(Path(settings.REF_CONFIGURATION) / "ref.toml")
 
 
-# Simple in-process cache to avoid repeatedly constructing Database/ProviderRegistry
-_DATABASE_CACHE: Database | None = None
-_PROVIDER_REGISTRY_CACHE: ProviderRegistry | None = None
-
 def get_database(ref_config: Config) -> Database:
     """
-    Get a shared database connection using the default config.
-
-    Caches the Database instance in-process to avoid repeatedly constructing
-    new engines/sessions per dependency resolution.
+    Get a new database connection using the default config
     """
-    global _DATABASE_CACHE
-    if _DATABASE_CACHE is None:
-        database = Database.from_config(ref_config, run_migrations=False)
-        with database._engine.connect() as connection:
-            if _get_database_revision(connection) is None:
-                raise ValueError(
-                    "The database migration has not been run. "
-                    "Check the database URL in your config file and run the migration."
-                )
-        _DATABASE_CACHE = database
-    return _DATABASE_CACHE
+    database = Database.from_config(ref_config, run_migrations=False)
+    with database._engine.connect() as connection:
+        if _get_database_revision(connection) is None:
+            raise ValueError(
+                "The database migration has not been run. "
+                "Check the database URL in your config file and run the migration."
+            )
+    return database
 
 
 def get_provider_registry(ref_config: Config) -> ProviderRegistry:
     """
     Get the provider registry
-
-    Caches the ProviderRegistry instance in-process to avoid re-loading providers
-    and duplicating database handles.
     """
-    global _PROVIDER_REGISTRY_CACHE
-    if _PROVIDER_REGISTRY_CACHE is None:
-        database = get_database(ref_config)
-        _PROVIDER_REGISTRY_CACHE = ProviderRegistry.build_from_config(ref_config, database)
-    return _PROVIDER_REGISTRY_CACHE
+    database = get_database(ref_config)
+    return ProviderRegistry.build_from_config(ref_config, database)
--- frontend/src/components/explorer/sourceExplorer.tsx
+++ frontend/src/components/explorer/sourceExplorer.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-// import { useSuspenseQuery } from "@tanstack/react-query";
-// import { diagnosticsFacetsOptions } from "@/client/@tanstack/react-query.gen";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { diagnosticsFacetsOptions } from "@/client/@tanstack/react-query.gen";
 import { Card, CardContent } from "@/components/ui/card.tsx";
 import { Route } from "@/routes/_app/explorer/sources";
 import { SourceExplorerContent } from "./sourceExplorerContent";
@@ -9,7 +9,7 @@ import { SourceSelect } from "./sourceSelect.tsx";
 
 export const SourceExplorer = () => {
   const { sourceId } = Route.useSearch();
-  // const { data } = useSuspenseQuery(diagnosticsFacetsOptions());
+  const { data } = useSuspenseQuery(diagnosticsFacetsOptions());
 
   return (
     <div className="space-y-4">
@@ -23,73 +23,7 @@ export const SourceExplorer = () => {
               Choose a model or dataset source. This will scope the explorer to
               results for the selected source.
             </p>
-            <SourceSelect
-              options={[
-                "ACCESS-CM2",
-                "ACCESS-ESM1-5",
-                "BCC-CSM2-MR",
-                "BCC-ESM1",
-                "CAMS-CSM1-0",
-                "CAS-ESM2-0",
-                "CESM2",
-                "CESM2-FV2",
-                "CESM2-WACCM",
-                "CESM2-WACCM-FV2",
-                "CIESM",
-                "CMCC-CM2-HR4",
-                "CMCC-CM2-SR5",
-                "CMCC-ESM2",
-                "CNRM-CM6-1",
-                "CNRM-CM6-1-HR",
-                "CNRM-ESM2-1",
-                "CanESM5",
-                "CanESM5-1",
-                "CanESM5-CanOE",
-                "E3SM-1-0",
-                "E3SM-1-1",
-                "E3SM-1-1-ECA",
-                "E3SM-2-0",
-                "E3SM-2-0-NARRM",
-                "E3SM-2-1",
-                "EC-Earth3-AerChem",
-                "EC-Earth3-CC",
-                "EC-Earth3-Veg",
-                "EC-Earth3-Veg-LR",
-                "FGOALS-f3-L",
-                "FGOALS-g3",
-                "FIO-ESM-2-0",
-                "GFDL-CM4",
-                "GFDL-ESM4",
-                "GISS-E2-1-G",
-                "GISS-E2-1-G-CC",
-                "GISS-E2-1-H",
-                "GISS-E2-2-G",
-                "GISS-E2-2-H",
-                "GISS-E3-G",
-                "IITM-ESM",
-                "INM-CM4-8",
-                "INM-CM5-0",
-                "IPSL-CM5A2-INCA",
-                "IPSL-CM6A-LR",
-                "IPSL-CM6A-LR-INCA",
-                "IPSL-CM6A-MR1",
-                "KACE-1-0-G",
-                "MCM-UA-1-0",
-                "MIROC-ES2H",
-                "MIROC-ES2L",
-                "MIROC6",
-                "MPI-ESM-1-2-HAM",
-                "MPI-ESM1-2-HR",
-                "MPI-ESM1-2-LR",
-                "MRI-ESM2-0",
-                "NESM3",
-                "NorCPM1",
-                "NorESM2-MM",
-                "SAM0-UNICON",
-                "TaiESM1",
-                "UKESM1-0-LL",
-              ]}
-            />
+            <SourceSelect options={data?.dimensions.source_id ?? []} />
           </div>
         </CardContent>
       </Card>
