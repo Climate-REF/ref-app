4c399019adbf1d54764be9d4e8a54b29fdd09413
Jared Lewis <jared@jared.kiwi.nz>
2025-10-07 15:26:50 +0200

    chore: Mark as not available

--- frontend/src/components/explorer/theme/earthSystem.tsx
+++ frontend/src/components/explorer/theme/earthSystem.tsx
@@ -60,8 +60,21 @@ const cards: ExplorerCard[] = [
         provider: "esmvaltool",
         diagnostic: "transient-climate-response-emissions",
         title: "Transient Climate Response to Emissions (TCRE)",
-        description:
-          "TCRE quantifies the change in global mean surface temperature per 1000 GtC of cumulative CO2 emissions. It reflects the near-linear relationship between cumulative carbon emissions and global warming, highlighting the direct impact of human activities on climate change. TCRE is a crucial metric for setting carbon budgets and informing climate policy, as it helps estimate the allowable emissions to limit global temperature rise to specific targets, such as those outlined in the Paris Agreement.",
+        description: (
+          <>
+            TCRE quantifies the change in global mean surface temperature per
+            1000 GtC of cumulative CO2 emissions. It reflects the near-linear
+            relationship between cumulative carbon emissions and global warming,
+            highlighting the direct impact of human activities on climate
+            change. TCRE is a crucial metric for setting carbon budgets and
+            informing climate policy, as it helps estimate the allowable
+            emissions to limit global temperature rise to specific targets, such
+            as those outlined in the Paris Agreement.
+            <span className="text-red-500">
+              Data to be available in next week
+            </span>
+          </>
+        ),
         interpretation:
           "Higher TCRE values indicate a more sensitive climate system, leading to greater warming for a given amount of CO2 emissions. This has significant implications for global temperature projections, sea level rise, and the frequency and intensity of extreme weather events.",
         metricUnits: "K/EgC",
@@ -75,8 +88,20 @@ const cards: ExplorerCard[] = [
         provider: "esmvaltool",
         diagnostic: "zero-emission-commitment",
         title: "Zero Emission Commitment (ZEC)",
-        description:
-          "ZEC represents the long-term change in global mean surface temperature following the cessation of CO2 emissions after a sustained period of increase. It reflects the balance between ongoing ocean heat uptake and the reduction in radiative forcing due to decreasing atmospheric CO2 levels. This metric is important for understanding the long-term climate implications of emission scenarios and for informing climate policy.",
+        description: (
+          <>
+            ZEC represents the long-term change in global mean surface
+            temperature following the cessation of CO2 emissions after a
+            sustained period of increase. It reflects the balance between
+            ongoing ocean heat uptake and the reduction in radiative forcing due
+            to decreasing atmospheric CO2 levels. This metric is important for
+            understanding the long-term climate implications of emission
+            scenarios and for informing climate policy.
+            <span className="text-red-500">
+              Data to be available in next week
+            </span>
+          </>
+        ),
         interpretation:
           "A negative ZEC indicates that the climate system has stabilizing feedbacks that help to counteract warming after emissions cease. Conversely, a positive ZEC suggests that the climate system may continue to warm even after emissions stop, highlighting the importance of early and sustained emission reductions to mitigate long-term climate impacts.",
         metricUnits: "K",
@@ -91,31 +116,7 @@ const cards: ExplorerCard[] = [
     title: "El Niño-Southern Oscillation (ENSO)",
     description:
       "Characteristics of ENSO, the dominant mode of interannual climate variability, which influences global weather patterns, precipitation, and temperature extremes.",
-    content: [
-      {
-        type: "box-whisker-chart",
-        provider: "esmvaltool",
-        diagnostic: "enso-basic-climatology",
-        title: "ENSO Basic Climatology",
-        groupingConfig: {
-          groupBy: "metric",
-          hue: "metric",
-        },
-        otherFilters: { region: "global" },
-      },
-      {
-        type: "box-whisker-chart",
-        provider: "pmp",
-        diagnostic: "enso_tel",
-        title: "ENSO Teleconnections",
-        description:
-          "ENSO teleconnections represent the far-reaching impacts of tropical Pacific SST variability on global climate patterns through atmospheric circulation changes. These include effects on North American winter climate, Indian monsoon, Australian rainfall, and Atlantic hurricane activity. Metrics assess how well models capture the spatial patterns and strength of these remote influences, typically evaluated through correlation or regression patterns. Proper representation of teleconnections is essential for seasonal prediction and understanding regional climate variability and change.",
-        groupingConfig: {
-          groupBy: "metric",
-          hue: "metric",
-        },
-      },
-    ],
+    content: [],
     placeholder: true,
   },
 ];
